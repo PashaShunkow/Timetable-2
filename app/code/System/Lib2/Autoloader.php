0aaaa5f25b4594a170e3fc2305cd4b30512ef050
Pasha Shunkow <shunkow@gmail.com>
2014-09-18 17:54:41 +0300

    Daily commit

---
+++ app/code/System/Lib2/Autoloader.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * System Autoloader
+ *
+ * @category  TimetableTool
+ * @package   TimetableTool
+ * @author    Paul Shunkow
+ * @copyright 2014 Paul Shunkow
+ */
+class Autoloader
+{
+    /**
+     * System autoloader
+     *
+     * @param string $class Class name
+     */
+    static public function autoload($class)
+    {
+        $classFilePath  = \App::getBaseDir();
+        $classPathParts = explode('\\', $class);
+        $classFilePath .= DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . 'code';
+
+
+        for ($i = 0; $i < count($classPathParts); $i++) {
+            $classFilePath .= DIRECTORY_SEPARATOR . $classPathParts[$i];
+        }
+
+        $classFilePath .= '.php';
+
+        if (is_file($classFilePath)) {
+            require_once $classFilePath;
+        }else{
+
+        }
+    }
+}
